461f9d8653d3735ffa626ae54c6800c6fa02a23e
matuesz-kleszcz <kleeeszcz@gmail.com>
2023-04-01 12:41:41 +0200

    fix: resolve conflicts

--- src/views/CreateRoomView.tsx
+++ src/views/CreateRoomView.tsx
@@ -1,36 +1,83 @@
-import {Card, styled, Typography} from "@mui/material";
+import { Card, styled } from "@mui/material";
 import { memo } from "react";
-import {ContainedButton} from "../components/Button";
-import {CustomSlider} from "../components/Slider";
-import {BodyTypography, TitleTypography} from "../components/Typography";
-import {OutlinedTextField} from "../components/TextField";
+import { ContainedButton } from "../components/Button";
+import { CustomSlider } from "../components/Slider";
+import { BodyTypography, TitleTypography } from "../components/Typography";
+import { useState } from "react";
+import { SERVER_URL } from "../const";
+import { Controller, useForm } from 'react-hook-form'
 
+
+enum CreateRoomError {
+  ServerError = 'server-error',
+  BadParams = 'bad-params',
+  Default = 'default'
+}
+
+type CreateRoomType = {
+  imageAmount: number;
+  roundAmount: number;
+}
 const StyledCard = styled(Card)({
-    display: "flex",
-    flexDirection: "column",
-    maxWidth: "800px",
-    margin: "auto",
-    padding: "20px",
-    "& > *": {
-        margin: "10px 0"
-    }
+  display: "flex",
+  flexDirection: "column",
+  maxWidth: "800px",
+  margin: "auto",
+  padding: "20px",
+  "& > *": {
+    margin: "10px 0"
+  }
 })
 
+const createRoom = async (data: CreateRoomType) => {
+  try {
+    console.log(data)
+    // const res = await fetch(`${SERVER_URL}/create-room`, {
+    //   method: 'POST',
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(data)
+    // })
+    // console.log(res)
+  } catch (e) {
+    return CreateRoomError.ServerError
+  }
+}
+
 export const CreateRoomView = memo(() => {
+
+  const { control, handleSubmit } = useForm<CreateRoomType>({
+    defaultValues: {
+      imageAmount: 5,
+      roundAmount: 5,
+    }
+  })
+
+  const [error, setError] = useState(CreateRoomError.Default)
+
   return (
     <StyledCard>
+      <form onSubmit={handleSubmit(createRoom)}>
         <TitleTypography>Create room</TitleTypography>
-
-        <OutlinedTextField label="Player Name" id="player-name"></OutlinedTextField>
-
         <BodyTypography>Number of photos</BodyTypography>
-        <CustomSlider defaultValue={4} max={20}/>
-
+        <Controller
+          control={control}
+          name="imageAmount"
+          render={({ field }) => (
+            <CustomSlider {...field} min={1} max={20} />
+          )}
+        />
         <BodyTypography>Time</BodyTypography>
-        <CustomSlider defaultValue={4} max={20}/>
-
-        <ContainedButton style={{marginLeft: "auto"}}>Submit</ContainedButton>
-
+        <Controller
+          control={control}
+          name="roundAmount"
+          render={({ field }) => (
+            <CustomSlider {...field} min={1} max={20} />
+          )}
+        />
+        <ContainedButton type="submit" style={{ marginLeft: "auto" }}>Submit</ContainedButton>
+      </form>
     </StyledCard>
   )
 })
