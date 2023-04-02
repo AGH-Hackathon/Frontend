320e3260026a5576f0ef4d5d82f513c3ea409ef6
nessel <maciej.nessel@gmail.com>
2023-04-02 14:38:58 +0200

    feat: add scores view

---
+++ src/views/ScoresView.tsx
@@ -0,0 +1,37 @@
+import { Card, styled } from "@mui/material";
+import { memo } from "react";
+import { Button } from "../components/Button";
+import { BodyTypography, SubtitleTypography, TitleTypography } from "../components/Typography";
+
+const StyledCard = styled(Card)(({ theme }) => ({
+  background: theme.palette.primary.main,
+  display: "flex",
+  flexDirection: "column",
+  margin: "auto",
+  width: theme.spacing(40),
+  padding: theme.spacing(4),
+  gap: theme.spacing(2),
+}))
+
+const scores = [
+  {
+    "username": "nick1",
+    "score": 5
+  },
+  {
+    "username": "nick2",
+    "score": 10
+  }
+]
+const totalScore = 20
+
+export const ScoresView = memo(() => {
+  return (
+    <StyledCard>
+      <TitleTypography align="center">Scores</TitleTypography>
+      <BodyTypography align="center">Max score: {totalScore}</BodyTypography>
+        {scores.map((data, i) => <SubtitleTypography>{i+1}. {data.username} <p style={{ display: 'inline-block', float: 'right', margin: 0 }}>{data.score}</p></SubtitleTypography>)}
+      <Button onClick={()=>{}} sx={{ width: 120, margin: 'auto' }}>Play Again</Button>
+    </StyledCard>
+  )
+})
